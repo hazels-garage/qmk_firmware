06aad06dda926b4a5020254f8f4c9169d9c8380c
Jason Hazel <jhazel@matrixmediaservices.com>
2023-06-22 20:18:31 -0400

    init

---
+++ keyboards/hazel/cut_slope/config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Jason Hazel (@jasonhazel)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#define SPI_DRIVER SPID1
+#define SPI_SCK_PIN GP14
+#define SPI_MISO_PIN GP12
+#define SPI_MOSI_PIN GP15
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
